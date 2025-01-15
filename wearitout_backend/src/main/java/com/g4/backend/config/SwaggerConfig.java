d2bccb73eef1d04ce7871c194d53fbac6e0e7509
Nguyễn Tuấn Hồng Đông Hải <94052742+Donghai0610@users.noreply.github.com>
2025-01-15 16:56:16 +0700

    ADD Swagger Config

---
+++ wearitout_backend/src/main/java/com/g4/backend/config/SwaggerConfig.java
@@ -0,0 +1,15 @@
+package com.g4.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Documentation")
+                        .version("1.0")
+                        .description("Swagger API documentation for WearItOut project"));
+    }
+}
