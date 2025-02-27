5f902d054d3106d7f0039da6548d6e768790f1c9
Nguyễn Tuấn Hồng Đông Hải <94052742+Donghai0610@users.noreply.github.com>
2025-02-27 07:50:38 +0700

    Add Dockerfile and intial deployment

--- wearitout_frontend/src/pages/ChatBoxPage.jsx
+++ wearitout_frontend/src/pages/ChatBoxPage.jsx
@@ -1,9 +1,41 @@
 import React from "react";
 import ChatBox from "../components/ChatBox";
+import ColorInit from "../helper/ColorInit";
+import ScrollToTop from "react-scroll-to-top";
+import Preloader from "../helper/Preloader";
+import BottomFooter from "../components/BottomFooter";
+import FooterTwo from "../components/FooterTwo";
+import HeaderTwo from "../components/HeaderTwo";
+import Breadcrumb from "../components/Breadcrumb";
+import NewsletterTwo from "../components/NewsletterTwo";
 const ChatBoxPage = () => {
     return (
         <>
-        <ChatBox/>
+            {/* ColorInit */}
+            <ColorInit color={true} />
+
+            {/* ScrollToTop */}
+            <ScrollToTop smooth color="#FA6400" />
+
+            {/* Preloader */}
+            <Preloader />
+
+            {/* HeaderTwo */}
+            <HeaderTwo category={true} />
+
+            {/* Breadcrumb */}
+            <Breadcrumb title={"Nhắn Tin"} />
+            {/* Checkout */}
+            <ChatBox />
+            {/* ShippingOne */}
+            <NewsletterTwo />
+            {/* FooterTwo */}
+            <FooterTwo />
+
+            {/* BottomFooter */}
+            <BottomFooter />
+
+
         </>
     );
 };
